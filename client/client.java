1296eef002deb965dbcab2a793ee6f10e83c31f5
bradleykenny <bkenny283@gmail.com>
2019-03-28 08:06:42 +1100

    Changes to get client working... receiving error from server.

--- client/client.java
+++ client/client.java
@@ -3,28 +3,27 @@ import java.io.*;
 
 public class client {
 	public static void main (String args[]) {
-		// arguments supply message and hostname of destination
 		Socket s = null;
 		try {
 			int serverPort = 8096;
 			s = new Socket(args[1], serverPort);
-			DataInputStream in = new DataInputStream( s.getInputStream());
-			DataOutputStream out = new DataOutputStream( s.getOutputStream());
-			out.writeUTF(args[0]); 
+			DataInputStream in = new DataInputStream(s.getInputStream());
+			DataOutputStream out = new DataOutputStream(s.getOutputStream());
+			out.writeUTF(args[0]);
 			String data = in.readUTF();
-			System.out.println("Received: "+ data) ;
+			System.out.println("Received: " + data) ;
 		} catch (UnknownHostException e) {
-			System.out.println("Sock:"+e.getMessage());
+			System.out.println("Sock:" + e.getMessage());
 		} catch (EOFException e) {
-			System.out.println("EOF:"+e.getMessage());
+			System.out.println("EOF:" + e.getMessage());
 		} catch (IOException e) {
-			System.out.println("IO:"+e.getMessage());
+			System.out.println("IO:" + e.getMessage());
 		} finally { 
-			if(s!=null) {
+			if (s != null) {
 				try { 
 					s.close();
 				} catch (IOException e) {
-					System.out.println("close:"+e.getMessage());
+					System.out.println("close:" + e.getMessage());
 				}
 			}
 		}
