99747dab4c35d5c7c1bf921cd4732b5317695946
bradleykenny <bkenny283@gmail.com>
2019-03-28 18:32:33 +1100

    Changing to BufferedReader.

--- client/gfgclient.java
+++ client/gfgclient.java
@@ -6,7 +6,7 @@ import java.io.*;
 public class gfgclient { 
     // initialize socket and input output streams 
     private Socket socket = null; 
-    private DataInputStream input = null; 
+    private BufferedReader input = null; 
     private DataOutputStream out = null; 
   
     // constructor to put ip address and port 
@@ -17,7 +17,7 @@ public class gfgclient {
             System.out.println("Connected"); 
   
             // takes input from terminal 
-            input = new DataInputStream(System.in); 
+            input = new BufferedReader(System.in); 
   
             // sends output to the socket 
             out = new DataOutputStream(socket.getOutputStream()); 
