7255a5a60c873a07a07df210f66f983443ca671a
bradleykenny <bkenny283@gmail.com>
2019-04-01 20:53:19 +1100

    Foundations for vallina product.

--- client/client.java
+++ client/client.java
@@ -1,32 +1,63 @@
-import java.net.*;
-import java.io.*;
-
-public class client {
-	public static void main (String args[]) {
-		Socket s = null;
-		try {
-			int serverPort = 8096;
-			s = new Socket(args[1], serverPort);
-			System.out.print("Yeet");
-			DataInputStream in = new DataInputStream(s.getInputStream());
-			DataOutputStream out = new DataOutputStream(s.getOutputStream());
-			out.writeUTF(args[0]);
-			String data = in.readUTF();
-			System.out.println("Received: " + data) ;
-		} catch (UnknownHostException e) {
-			System.out.println("Sock:" + e.getMessage());
-		} catch (EOFException e) {
-			System.out.println("EOF:" + e.getMessage());
-		} catch (IOException e) {
-			System.out.println("IO:" + e.getMessage());
-		} finally { 
-			if (s != null) {
-				try { 
-					s.close();
-				} catch (IOException e) {
-					System.out.println("close:" + e.getMessage());
-				}
-			}
-		}
+import java.net.*; 
+import java.io.*; 
+import java.util.*;
+  
+public class client { 
+    private Socket socket = null; 
+    private DataInputStream input = null; 
+    private DataOutputStream output = null; 
+  
+    public client(String address, int port) { 
+        // establish a connection 
+        try { 
+            socket = new Socket(address, port); 
+            System.out.println("Connected."); 
+  
+            input = new DataInputStream(System.in); 
+            output = new DataOutputStream(socket.getOutputStream()); 
+        } 
+        catch(UnknownHostException u) { 
+            System.out.println("ERR: " + u); 
+        } 
+        catch(IOException i) { 
+            System.out.println("ERR: " + i); 
+        } 
+  
+        // DEBUGGING ONLY
+        String line = ""; 
+        while (!line.equals("QUIT")) { 
+            try { 
+                line = input.readLine();
+                output.write(line.getBytes());
+            } 
+            catch(IOException i) { 
+                System.out.println("IOE: " + i); 
+            } 
+        } 
+  
+        // close the connection 
+        try { 
+            input.close(); 
+            output.close(); 
+            socket.close(); 
+        } 
+        catch(IOException i) { 
+            System.out.println("ERR: " + i); 
+        } 
+	} 
+	
+	// TODO: method to send information to the server
+	public void send() {
+		//
 	}
-}
+
+	// TODO: method to receive information from the server
+	public void receive() {
+		//
+	}
+	
+	// main method that runs
+    public static void main(String args[]) { 
+        client ourClient = new client("127.0.0.1", 8096); 
+    } 
+} 
