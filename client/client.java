c4def634c1a29ba7ade4d9194698f030e88c3b3b
John K <Callidusjohn@users.noreply.github.com>
2019-04-03 10:55:18 +1100

    Half working RCVD

--- client/client.java
+++ client/client.java
@@ -2,86 +2,118 @@ import java.net.*;
 import java.io.*; 
 import java.util.*;
   
-public class client { 
+public class client 
+{ 
     private Socket socket = null; 
     private BufferedReader input = null; // USED GET INFO FROM SOCKET
     private DataOutputStream output = null; // USED TO WRITE TO SOCKET
     
-    public client(String address, int port) { 
+    public client(String address, int port) 
+    { 
         // CREATE CONNECTION 
-        try { 
+        try 
+        { 
             socket = new Socket(address, port); 
             System.out.println("Connected."); 
   
             input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             output = new DataOutputStream(socket.getOutputStream()); 
         } 
-        catch(UnknownHostException u) { 
+        catch(UnknownHostException u) 
+        { 
             System.out.println("ERR: " + u); 
         } 
-        catch(IOException i) { 
+        catch(IOException i)
+        { 
             System.out.println("ERR: " + i); 
         } 
 
         send(output, "HELO");
+        
         String received = receive(input);
-        System.out.println("RECD: " + received);
+
+        send(output, "AUTH john");
+
+        System.out.println("RCVD: " + received);
         
         // DEBUGGING ONLY
-        debug(output);
+        //debug(output);
   
         // CLOSE CONNECTION
-        try { 
+        try 
+        { 
             input.close(); 
             output.close(); 
             socket.close(); 
         } 
-        catch(IOException i) { 
+        catch(IOException i) 
+        { 
             System.out.println("ERR: " + i); 
         } 
 	} 
 	
 	// SENDING MESSAGES TO THE SERVER
-	public void send(DataOutputStream destination, String message) {
-        try {
+    public void send(DataOutputStream destination, String message) 
+    {
+        try 
+        {
             destination.write(message.getBytes());
             System.out.println("SENT: " + message);
-        } catch (IOException i) {
+        } 
+        catch (IOException i) 
+        {
             System.out.println("ERR: " + i);
         }
 	}
 
 	// RECEIVING MESSAGES FROM THE SOCKET
-	public String receive(BufferedReader input) {
+    public String receive(BufferedReader input) 
+    {
         String message = "";
-        try {
-            while (message == "") {
-                message = input.readLine();
-                System.out.println("MESSAGE: " + message);
+        try 
+        {
+            if (input.ready())
+            {
+                while ((message = input.readLine()) != null)
+                {
+                    // this prints out ok
+                    System.out.flush();
+                    System.out.println("MESSAGE: " + message);
+                }
             }
-        } catch(IOException i) {
+
+        } 
+        catch(IOException i) 
+        {
             System.out.println("ERR: " + i);
         } 
-        return message;
+        // this prints out null
+        System.out.flush();
+        return message + '\n';
     }
     
     // DEBUGGING VIA MANUAL INPUT/OUTPUT
-    public void debug(DataOutputStream output) {
+    public void debug(DataOutputStream output) 
+    {
         DataInputStream man_input = new DataInputStream(System.in); 
         String line = ""; 
-        while (!line.equals("QUIT")) { 
-            try { 
+        while (!line.equals("QUIT")) 
+        { 
+            try 
+            { 
                 line = man_input.readLine();
                 output.write(line.getBytes());
             } 
-            catch(IOException i) { 
+            catch(IOException i) 
+            { 
                 System.out.println("IOE: " + i); 
             } 
         } 
     }
 	
 	// THIS IS WHAT RUNS
-    public static void main(String args[]) { 
+    public static void main(String args[]) 
+    { 
         client ourClient = new client("127.0.0.1", 8096); 
     }
 }
