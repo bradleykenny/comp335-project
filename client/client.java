01c107d58972a0b9fcc3546d7bdd1f72f02f32b2
bradleykenny <bkenny283@gmail.com>
2019-04-03 13:11:02 +1100

    Finally got received() working.

--- client/client.java
+++ client/client.java
@@ -2,29 +2,24 @@ import java.net.*;
 import java.io.*; 
 import java.util.*;
   
-public class client 
-{ 
+public class client { 
 	private Socket socket = null; 
 	private BufferedReader input = null; // USED GET INFO FROM SOCKET
 	private DataOutputStream output = null; // USED TO WRITE TO SOCKET
 	
-	public client(String address, int port) 
-	{ 
+	public client(String address, int port) { 
 		// CREATE CONNECTION 
-		try 
-		{ 
+		try { 
 			socket = new Socket(address, port); 
 			System.out.println("Connected."); 
   
 			input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			output = new DataOutputStream(socket.getOutputStream()); 
 		} 
-		catch(UnknownHostException u) 
-		{ 
+		catch(UnknownHostException u) { 
 			System.out.println("ERR: " + u); 
 		} 
-		catch(IOException i)
-		{ 
+		catch(IOException i) { 
 			System.out.println("ERR: " + i); 
 		} 
 
@@ -40,51 +35,46 @@ public class client
 		//debug(output);
   
 		// CLOSE CONNECTION
-		try 
-		{ 
+		try { 
 			input.close(); 
 			output.close(); 
 			socket.close(); 
 		} 
-		catch(IOException i) 
-		{ 
+		catch(IOException i) { 
 			System.out.println("ERR: " + i); 
 		} 
 	} 
 	
 	// SENDING MESSAGES TO THE SERVER
-	public void send(DataOutputStream destination, String message) 
-	{
-		try 
-		{
+	public void send(DataOutputStream destination, String message) {
+		try {
 			destination.write(message.getBytes());
 			System.out.println("SENT: " + message);
 		} 
-		catch (IOException i) 
-		{
+		catch (IOException i) {
 			System.out.println("ERR: " + i);
 		}
 	}
 
 	// RECEIVING MESSAGES FROM THE SOCKET
-	public String receive(BufferedReader input) 
-	{
+	public String receive(BufferedReader input) {
+		int singleChar = 0;
 		String message = "";
-		try 
-		{
-			if (input.ready()) 
-			{
-				while ((message = input.readLine()) != null)
-				{
-					// this prints out ok
-					System.out.flush();
-					System.out.println("MESSAGE: " + message);
+		try {
+			if (input.ready()) {
+				try {
+					System.out.println("RCVD: ");
+					while ((singleChar = input.read()) != -1) {
+						System.out.print((char) singleChar);
+						message += (char) singleChar;
+					}
+				} catch (IOException i) {
+					System.out.println("ERR: " + i);
 				}
 			}
 
 		} 
-		catch(IOException i) 
-		{
+		catch(IOException i) {
 			System.out.println("ERR: " + i);
 		} 
 		// this prints out null
@@ -93,27 +83,22 @@ public class client
 	}
 	
 	// DEBUGGING VIA MANUAL INPUT/OUTPUT
-	public void debug(DataOutputStream output) 
-	{
+	public void debug(DataOutputStream output) {
 		DataInputStream man_input = new DataInputStream(System.in); 
 		String line = ""; 
-		while (!line.equals("QUIT")) 
-		{ 
-			try 
-			{ 
+		while (!line.equals("QUIT")) { 
+			try { 
 				line = man_input.readLine();
 				output.write(line.getBytes());
 			} 
-			catch(IOException i) 
-			{ 
+			catch(IOException i) { 
 				System.out.println("IOE: " + i); 
 			} 
 		} 
 	}
 	
 	// THIS IS WHAT RUNS
-	public static void main(String args[]) 
-	{ 
+	public static void main(String args[]) { 
 		client ourClient = new client("127.0.0.1", 8096); 
 	}
 }
