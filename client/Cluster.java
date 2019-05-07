74f90466cba0db903f9e84ff81e703c99c62501a
John K <Callidusjohn@users.noreply.github.com>
2019-05-07 14:27:39 +1000

    ff interpretation

--- client/Cluster.java
+++ client/Cluster.java
@@ -11,6 +11,16 @@ public class Cluster {
 	}
 
 	public void firstFit() {
-		
+		//
+		// A job gets read; read server state info: 
+		// type | id | state | time | cores | memory | space
+		// FOR each server type, from smallest to largest
+		// 		FOR each server
+		// 			IF server has enough cores to run the job
+		// 				RETURN server
+		// 			END IF
+		// 		END FOR
+		// END FOR
+		// RETURN first "Active" server with enough cores (resource capacity) ro run the job
 	}
 }
